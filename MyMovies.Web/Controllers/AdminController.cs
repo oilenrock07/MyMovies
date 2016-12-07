7f334538f23aebb76036ba82385eecf1dc0e83d8
Cornelio Cawicaan Jr <Cornelio.Cawicaan@nowcom.com.ph>
2016-12-07 13:08:08 +0800

    Initial commit for saving admin movie maintenance

--- MyMovies.Web/Controllers/AdminController.cs
+++ MyMovies.Web/Controllers/AdminController.cs
@@ -3,18 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyMovies.Common.Extension;
+using MyMovies.Entities;
+using MyMovies.Infrastructure.Interfaces;
+using MyMovies.Repository.Interfaces;
+using MyMovies.Web.ViewModels;
+using Omu.ValueInjecter;
 
 namespace MyMovies.Web.Controllers
 {
     public class AdminController : Controller
     {
-        //
-        // GET: /Admin/
+        private readonly IMovieRepository _movieRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public ActionResult Index()
+        public AdminController(IMovieRepository movieRepository, IUnitOfWork unitOfWork)
         {
-            return View();
+            _movieRepository = movieRepository;
+            _unitOfWork = unitOfWork;
         }
 
+        public ActionResult Index(int id = 0)
+        {
+            var viewModel = new MovieViewModel();
+            if (id > 0)
+            {
+                var movie = _movieRepository.GetById(id);
+                viewModel = movie.MapItem<MovieViewModel>();
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult MovieMaintenance(MovieViewModel viewModel)
+        {
+            var id = viewModel.MovieId;
+            try
+            {                
+                if (viewModel.MovieId == 0)
+                {
+                    var movie = viewModel.MapItem<Movie>();
+                    _movieRepository.Add(movie);
+                    _unitOfWork.Commit();
+                    id = movie.MovieId;
+
+                    TempData["Save"] = MyMovieResource.MoviedAdded;
+                }
+                else
+                {
+                    var movie = _movieRepository.GetById(viewModel.MovieId);
+                    _movieRepository.Update(movie);
+                    movie.InjectFrom(viewModel);
+
+                    _unitOfWork.Commit();
+
+                    TempData["Save"] = MyMovieResource.MovieUpdated;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = MyMovieResource.MovieErrorSaving;
+            }
+
+            return RedirectToAction("Index", new { id });
+        }
     }
 }
--- MyMovies.Web/ViewModels/MovieViewModel.cs
+++ MyMovies.Web/ViewModels/MovieViewModel.cs
@@ -6,10 +6,30 @@ namespace MyMovies.Web.ViewModels
 {
     public class MovieViewModel : Movie
     {
-        public IEnumerable<string> StarLists => Stars.Split(',').Select(x => x.Trim());
-        public IEnumerable<string> GenreList => Genre.Split(',').Select(x => x.Trim());
+        public IEnumerable<string> StarLists
+        {
+            get
+            {
+                return Stars.Split(',').Select(x => x.Trim());
+            }
+        }
+
+        public IEnumerable<string> GenreList
+        {
+            get
+            {
+                return Genre.Split(',').Select(x => x.Trim());
+            } 
+        } 
+
         public IList<Movie> RelatedMovies { get; set; }
 
-        public string FormattedRate => Rate.ToString("#.#");
+        public string FormattedRate
+        {
+            get
+            {
+                return Rate.ToString("#.#");
+            }
+        } 
     }
 }
