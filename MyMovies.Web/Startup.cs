c4a80cc2904da4ea4337e94a6fca5f9689e74657
Cornelio Cawicaan Jr <Cornelio.Cawicaan@nowcom.com.ph>
2016-12-14 14:29:59 +0800

    Fix Owin Setup

---
+++ MyMovies.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using MyMovies.Web;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace MyMovies.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
