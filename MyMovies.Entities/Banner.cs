43f54d061484d033418ad979794a0e207bdd217a
Cornelio Cawicaan Jr <Cornelio.Cawicaan@nowcom.com.ph>
2017-03-28 14:31:05 +0800

    Added banner class

---
+++ MyMovies.Entities/Banner.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MyMovies.Entities.Enums;
+
+namespace MyMovies.Entities
+{
+    public class Banner
+    {
+        [Key]
+        public int BannerId { get; set; }
+
+        public string Poster { get; set; }
+
+        public string TextColor { get; set; }
+
+        public string Identifier { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public BannerType Type { get; set; }
+    }
+}
